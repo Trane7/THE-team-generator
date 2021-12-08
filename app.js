3b0d21bd979a7180ecea24fc44bb5051ae20f78c
Dee <dgrowette@gmail.com>
2021-12-08 15:52:30 -0800

    filled in app.js

---
+++ app.js
@@ -0,0 +1,257 @@
+const Manager = require('./lib/Manager')
+const Intern = require('./lib/Intern')
+const Engineer = require('./lib/Engineer')
+const inquirer = require("inquirer")
+const path = require("path")
+const fs = require("fs")
+
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html")
+
+const render = require("./lib/htmlRenderer")
+
+
+const team = []
+
+
+const promptManager = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter you name here (Required):',
+            validate: nameInput => {
+                if(nameInput) {
+                    return(true)
+                } else {
+                    console.log('Please enter name to proceed!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'employeeId',
+            message: 'Enter employee ID (Required):',
+            validate: employeeId => {
+                if (employeeId) {
+                    return true
+                } else {
+                    console.log('Please enter employee ID to proceed!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter email (Required):',
+            validate: email => {
+                if (email) {
+                    return true
+                } else {
+                    console.log('Please enter email to proceed!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'Enter your office number (Required):',
+            validate: officeNumber => {
+                if (officeNumber) {
+                    return true
+                } else {
+                    console.log('Please enter office number to proceed!')
+                    return false
+                }
+            }
+        },
+    ])
+    .then(answers => {
+        console.log(answers)
+        const manager = new Manager(answers.name, answers.employeeId, answers.email, answers.officeNumber)
+        team.push(manager)
+        promptMain()
+    })
+}
+
+const promptMain = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'main',
+            message: 'Please choose from the following actions:',
+            choices: ('add an intern', 'add an engineer', 'done')
+
+        }
+    ])
+    .then(userChoice => {
+        switch (userChoice.main) {
+            case "add an intern":
+                promptIntern()
+                break
+            case "add an engineer":
+                promptEngineer()
+                break
+            default:
+                buildRoster()        
+        }
+    })
+}
+
+const promptIntern = () => {
+    console.log(`
+    ================
+    Add a New Intern
+    ================
+    `)
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Name of intern? (Required):',
+            validate: internName => {
+                if (internName) {
+                    return true
+                } else {
+                    console.log('Please enter interns name to proceed!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'employeeId',
+            message: 'Enter employees ID number (Required):',
+            validate: employeeId => {
+                if (employeeId) {
+                    return true
+                } else {
+                    console.log('Please enter employee ID to proceed!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter email (Required):',
+            validate: email => {
+                if (email) {
+                    return true
+                } else {
+                    console.log('Please enter email to proceed!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Name of interns school (Required):',
+            validate: school => {
+                if (school) {
+                    return true
+                } else {
+                    console.log('Please enter interns school to proceed!')
+                    return false
+                }
+            }
+        }
+    ])
+    .then(answers => {
+        console.log(answers)
+        const intern = new Intern(answers.name, answers.employeeId, answers.email, answers.school)
+        team.push(intern)
+        promptMain()
+    })
+}
+
+
+const promptEngineer = () => {
+    console.log(`
+    ==================
+    Add a New Engineer
+    ==================
+    `)
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Name of engineer? (Required):',
+            validate: engineerName => {
+                if (engineerName) {
+                    return true
+                } else {
+                    console.log('Please enter engineers name to proceed!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'employeeId',
+            message: 'Enter employees ID number (Required):',
+            validate: employeeId => {
+                if (employeeId) {
+                    return true
+                } else {
+                    console.log('Please enter employee ID to proceed!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter email (Required):',
+            validate: email => {
+                if (email) {
+                    return true
+                } else {
+                    console.log('Please enter email to proceed!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'githubUN',
+            message: 'Name of engineers github username? (Required):',
+            validate: githubUN => {
+                if (githubUN) {
+                    return true
+                } else {
+                    console.log('Please enter engineers github username to proceed!')
+                    return false
+                }
+            }
+        }
+    ])
+    .then(answers => {
+        console.log(answers)
+        const engineer = new Intern(answers.name, answers.employeeId, answers.email, answers.githubUN)
+        team.push(engineer)
+        promptMain()
+    })
+}
+
+const buildRoster = () => {
+    console.log(`
+    ===============
+    Finished Roster
+    ===============
+    `);
+
+    // Builds an output if one doesn't currently exsit
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR)
+    }
+    fs.writeFileSync(outputPath, generateSite(teamRoster), "utf-8")
+}
+
+promptManager()
