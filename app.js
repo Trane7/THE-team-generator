40b860ff028e5611a6afa476d3e23ed90f05901c
Dee <dgrowette@gmail.com>
2021-12-08 21:15:42 -0800

    added to generate-page.js

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const inquirer = require("inquirer")
 const Manager = require('./lib/Manager')
 const Intern = require('./lib/Intern')
 const Engineer = require('./lib/Engineer')
+const generatePage = require('./src/generate-page')
 const path = require("path")
 const fs = require("fs")
 
 const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "roster.html")
+const outputPath = path.join(OUTPUT_DIR, )
 const roster = []
 
 
---
+++ src/generate-page.js
@@ -0,0 +1,13 @@
+class generatePage {
+    
+}
+
+
+
+
+
+
+
+
+
+module.exports = generatePage
